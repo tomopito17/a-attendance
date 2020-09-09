69d00d8ba3015954433b50f0f27419a5d10118b5
tomopito17 <tomopito17@gmail.com>
2020-09-10 00:31:05 +0900

    repo

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200508103205) do
+ActiveRecord::Schema.define(version: 2020_05_08_103205) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -32,9 +32,10 @@ ActiveRecord::Schema.define(version: 20200508103205) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-08-23 23:00:00"
-    t.datetime "work_time", default: "2020-08-23 22:30:00"
+    t.datetime "basic_time", default: "2020-09-08 23:00:00"
+    t.datetime "work_time", default: "2020-09-08 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "attendances", "users"
 end
